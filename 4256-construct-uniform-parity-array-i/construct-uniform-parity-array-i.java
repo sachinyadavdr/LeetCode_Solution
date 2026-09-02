6692166533b5470afa0486be99eff5680ea22ed9
sachin <sachinyadav894848@gmail.com>
2026-09-02 22:54:18 +0530

    Time: 1 ms (31.89%) | Memory: 45 MB (87.60%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -2,18 +2,37 @@ class Solution {
     public boolean uniformArray(int[] nums1) {
         int n=nums1.length;
         int nums2[]=new int[n];
-        for(int i=0;i<n-1;i++){
-            if(nums1[i+1]%2!=0){
-                nums2[i]=nums1[i]-nums1[i+1];
-
+        int odd=-1;
+        for(int i=0;i<n;i++){
+            if(nums1[i]%2!=0){
+                odd=nums1[i];
+            break;
+            }
+        } 
+        if(odd!=-1){
+            for(int i=0;i<n;i++){
+            if(nums1[i]%2!=0){
+            nums2[i]=nums1[i];
+            
             }else{
-                nums2[i]=nums1[i];
+                nums2[i]=nums1[i]-odd;
             }
-        } for(int x:nums2){
-            if(x%2==0){
-                return true;
+
+
             }
-           
-        }return false;
+        }
+            else{
+              for(int i=0;i<n;i++){
+        nums2[i]=nums1[i];
+    }
+                }
+            
+            boolean even=nums2[0]%2==0;
+        for(int x:nums2){
+            if((x%2==0)!=even){
+                return false;
+            }
+        }
+    return true;
     }
 }
