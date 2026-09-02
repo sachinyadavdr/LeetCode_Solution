fddaeafaf99c19f011aeee59f375c6eb53d61fbd
sachin <sachinyadav894848@gmail.com>
2026-09-02 22:29:50 +0530

    Time: 1 ms (31.89%) | Memory: 45.2 MB (35.83%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        return true;
+        int n=nums1.length;
+        int nums2[]=new int[n];
+        for(int i=0;i<n-1;i++){
+            if(nums1[i+1]%2!=0){
+                nums2[i]=nums1[i]-nums1[i+1];
+
+            }else{
+                nums2[i]=nums1[i];
+            }
+        } for(int x:nums2){
+            if(x%2==0){
+                return true;
+            }
+            if(x%2!=0){
+                return true;
+            }
+        }return false;
     }
 }
