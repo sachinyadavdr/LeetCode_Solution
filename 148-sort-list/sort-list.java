b359c66d7dd15e4f75ce3d0c5b8beb09c7f20295
sachin <sachinyadav894848@gmail.com>
2026-09-08 14:05:24 +0530

    Time: 14 ms (22.06%) | Memory: 66.6 MB (5.86%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,30 +10,28 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode tem = head;
+        ListNode t=head;
         int size=0;
-        while(tem!=null){
+        while(t!=null){
             size++;
-            tem=tem.next;
+            t=t.next;
         }
-        tem=head;
-        int[] arr = new int[size];
-        int i = 0;
-        while (tem != null) {
-            arr[i] = tem.val;
-            tem = tem.next;
-            i++;
+        t=head;
+        int arr[]=new int [size];
+        for(int i=0;i<size;i++){
+            arr[i]=t.val;
+            t=t.next;
         }
         Arrays.sort(arr);
-        int n = arr.length;
-        ListNode dummy = new ListNode(-1);
-        ListNode t = dummy;
-        for (int j = 0; j < n; j++) {
-            t.next = new ListNode(arr[j]);
-            t=t.next;
-
+         
+        ListNode dummy=new ListNode(-1);
+        ListNode tem=dummy;
+        int i=0;
+        while(i<size){
+            tem.next=new ListNode(arr[i]);
+            tem=tem.next;
+            i++;
         }
-
-        return dummy.next;
-    }
+        return   dummy.next;
+    } 
 }
